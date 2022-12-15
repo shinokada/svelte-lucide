e2b45be603dc219068d53ee46c990f9c3940fe15
Shinichi Okada <okada.shin@gmail.com>
2022-12-15 20:49:22 +0900

    fix: sveltekit 1.0.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,15 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-  },
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-  ],
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
