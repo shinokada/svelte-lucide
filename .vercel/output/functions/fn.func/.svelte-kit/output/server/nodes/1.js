ace1d6548e6ff84ef490f74ba6df51eeee25db45
Shinichi Okada <okada.shin@gmail.com>
2023-07-18 19:18:21 +0200

    fix: update flowbite-svelte v0.40.1, add @floating-ui/dom remove flowbite and popper, add tests

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.cb8fe193.js","_app/immutable/chunks/scheduler.b0e64abc.js","_app/immutable/chunks/index.8487da62.js","_app/immutable/chunks/stores.00d3664a.js","_app/immutable/chunks/singletons.f094ebec.js","_app/immutable/chunks/index.97201a03.js"];
+export const imports = ["_app/immutable/nodes/1.a5020c59.js","_app/immutable/chunks/scheduler.b0e64abc.js","_app/immutable/chunks/index.8487da62.js","_app/immutable/chunks/stores.b2d050a7.js","_app/immutable/chunks/singletons.f0484021.js","_app/immutable/chunks/index.97201a03.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","css/vue.css","favicon-16x16.png","favicon-32x32.png","favicon.ico","images/lucide-650-1050-optimized.png","images/lucide-650-1050.png","images/lucide-optimized.png","images/lucide.png","images/lucide1.webp","images/lucide2.webp","images/lucide3.webp","images/lucide4.webp","maskable_icon_x192.png","styles/docs.css"]),
 	mimeTypes: {".png":"image/png",".css":"text/css",".ico":"image/vnd.microsoft.icon",".webp":"image/webp"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.000ac199.js","app":"_app/immutable/entry/app.e7d78e32.js","imports":["_app/immutable/entry/start.000ac199.js","_app/immutable/chunks/scheduler.b0e64abc.js","_app/immutable/chunks/singletons.f094ebec.js","_app/immutable/chunks/index.97201a03.js","_app/immutable/entry/app.e7d78e32.js","_app/immutable/chunks/scheduler.b0e64abc.js","_app/immutable/chunks/index.8487da62.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.4eadf72b.js","app":"_app/immutable/entry/app.5e79883a.js","imports":["_app/immutable/entry/start.4eadf72b.js","_app/immutable/chunks/scheduler.b0e64abc.js","_app/immutable/chunks/singletons.f0484021.js","_app/immutable/chunks/index.97201a03.js","_app/immutable/entry/app.5e79883a.js","_app/immutable/chunks/scheduler.b0e64abc.js","_app/immutable/chunks/index.8487da62.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
