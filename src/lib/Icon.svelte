d4f364df23dc7204fec3dfaeb331e605ac958b34
Shinichi Okada <okada.shin@gmail.com>
2023-07-31 17:52:48 +0200

    fix: component doc update

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -40,7 +40,7 @@ viewBox="0 0 {displayIcon.box} {displayIcon.box}"
 
 <!--
 @component
-[Go to Document](https://shinokada.github.io/svelte-file-icons/)
+[Go to Document](https://shinokada.github.io/svelte-lucide/)
 ## Props
 @prop name;
 @prop width = "24";
