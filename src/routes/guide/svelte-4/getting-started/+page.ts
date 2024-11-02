06230e1f6897b3c7bdc77131d95a2f7f1ed98fe0
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-11-02 16:12:14 +0100

    fix: lint fixes

--- src/routes/guide/svelte-4/getting-started/+page.ts
+++ src/routes/guide/svelte-4/getting-started/+page.ts
@@ -4,7 +4,7 @@ const title = 'Getting Started - Svelte Lucide v1';
 const description = 'How to get started with Svelte Lucide v1';
 const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-lucide';
 
-export const load = ({ url }) => {
+export const load = () => {
   const pageMetaTags: MetaProps = {
     title,
     description,
--- src/routes/guide/svelte-5/getting-started/+page.ts
+++ src/routes/guide/svelte-5/getting-started/+page.ts
@@ -4,7 +4,7 @@ const title = 'Getting Started - Svelte Lucide v2';
 const description = 'How to get started with Svelte Lucide v2';
 const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-lucide';
 
-export const load = ({ url }) => {
+export const load = () => {
   const pageMetaTags: MetaProps = {
     title,
     description,
